f9fd3fb138d65a164b0e18b97f0a38a5fbf02295
Ahmed-Nasser99 <ahmednsmd@gmail.com>
2024-08-03 20:37:50 +0300

    Update I18n and setup arabic to be init language

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,16 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import translationAr from "./locales/ar/translation.json";
+import translationEn from "./locales/en/translation.json";
+const resources = {
+  en: {
+    translation: translationEn,
+  },
+  ar: {
+    translation: translationAr,
+  },
+};
 
 i18n
   .use(HttpApi)
@@ -9,14 +19,12 @@ i18n
   .use(initReactI18next)
   .init({
     supportedLngs: ["en", "ar"],
-    fallbackLng: "ar",
+    lng: "ar",
     detection: {
       order: ["cookie", "localStorage", "htmlTag", "path", "subdomain"],
       caches: ["cookie"],
     },
-    backend: {
-      loadPath: "/locales/{{lng}}/translation.json",
-    },
+    resources,
     react: { useSuspense: false },
   });
 
